4ebaed5ad8e311426a9cc41167cf6a8f517c2007
Marks <meshkis.mark@gmail.com>
2022-01-26 23:14:53 +0200

    Main daļa uzsākšana

---
+++ src/picerija/PizzaProgramm.java
@@ -0,0 +1,16 @@
+package picerija;
+
+import javax.swing.JOptionPane;
+
+public class PizzaProgramm {
+	
+	public static void welcomeMessage(){
+		JOptionPane.showMessageDialog(null, "Sveicinati picu pasutisanas programma!");
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
