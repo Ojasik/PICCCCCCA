533a77b448dfb862997fc8e5112f1c4379e321d5
Marks <meshkis.mark@gmail.com>
2022-01-26 15:30:28 +0200

    Dzeriens klase papildinjums

--- src/picerija/Dzeriens.java
+++ src/picerija/Dzeriens.java
@@ -1,5 +1,25 @@
 package picerija;
 
-public class Dzeriens {
+import java.util.ArrayList;
 
+public class Dzeriens {
+	
+	private int tilp;
+	private ArrayList<String> dzerieni;
+	
+	public ArrayList<String> getDzeriens() {
+		return dzerieni;
+	}
+	
+	public void setDzeriens(ArrayList<String> dzerieni) {
+		this.dzerieni=dzerieni;
+	}
+	
+	public int getTilp() {
+		return tilp;
+	}
+	
+	public void setTilp(int tilp) {
+		this.tilp=tilp;
+	}
 }
