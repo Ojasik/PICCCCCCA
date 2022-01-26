467c3e546ec4c30b00a28bad28dbe1c035e3b5f9
Marks <meshkis.mark@gmail.com>
2022-01-27 00:36:48 +0200

    Order calculation izveidošana

--- src/picerija/Order.java
+++ src/picerija/Order.java
@@ -10,5 +10,18 @@ public class Order {
 	public Order(ArrayList<Pizza> pizzas) {
 		this.pizzas=pizzas;
 	}
+	
+	public void orderPriceCalculation() {
+		final double LargeCost = 9.99;
+		final double MediumCost = 7.99;
+		final double SmallCost = 5.99;
+		final double siers = 2.00;
+		final double bekons = 1.50;
+		final double ananasi = 0.70;
+		final double vistasfileja = 2.50;
+		final double maltagala = 2.20;
+		final double sampinjoni = 0.80;
+		
+	}
 
 }
